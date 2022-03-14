fa0ca954d254c429b3d8b31225a4ccf8b8ceadfb
acancor145407 <98743495+acancor145407@users.noreply.github.com>
2022-03-14 11:56:16 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+function girar90derecha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+    basic.pause(1100)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+function girar90izquierda () {
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    basic.pause(1100)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+        maqueen.motorStop(maqueen.Motors.All)
+        girar90derecha()
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    }
+})
