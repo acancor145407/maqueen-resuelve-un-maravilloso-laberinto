f1edefbc50a5797715db4f92bd142da2a442ce4f
acancor145407 <98743495+acancor145407@users.noreply.github.com>
2022-03-16 09:27:14 +0100

    holi

--- main.ts
+++ main.ts
@@ -16,14 +16,18 @@ function girar90izquierda () {
 }
 let ultimogiro = 0
 basic.forever(function () {
+	
+})
+basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
         maqueen.motorStop(maqueen.Motors.All)
-        if (ultimogiro == 0) {
-            girar90izquierda()
-            ultimogiro += 1
-        } else {
+        girar90izquierda()
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+            maqueen.motorStop(maqueen.Motors.All)
             girar90derecha()
-            ultimogiro = 0
+        }
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+            girar90derecha()
         }
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
